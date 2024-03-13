0dc12cefffa59537fffbfade75a169a4f2cafc7a
einfachleni23 <160415319+einfachleni23@users.noreply.github.com>
2024-03-13 10:57:28 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,7 +2,7 @@ const generateForm = document.querySelector(".generate-form");
 const generateBtn = generateForm.querySelector(".generate-btn");
 const imageGallery = document.querySelector(".image-gallery");
 
-const OPENAI_API_KEY = "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"; // Your OpenAI API key here
+const OPENAI_API_KEY = "OPENAI_API_KEY"; // Your OpenAI API key here
 let isImageGenerating = false;
 
 const updateImageCard = (imgDataArray) => {
